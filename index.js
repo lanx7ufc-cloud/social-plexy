b4b0944c561fda5aa270f5bf1aa820c6bc727295
lanx7ufc-cloud <lanx7ufc@gmail.com>
2026-05-09 17:05:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -39,56 +39,47 @@ await page.setUserAgent(
 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122 Safari/537.36"
 );
 
+let videoUrl = null;
+
+// captura requests
+page.on("response", async(response)=>{
+
+try{
+
+const resUrl = response.url();
+
+if(
+resUrl.includes(".mp4") &&
+resUrl.includes("pinimg")
+){
+
+videoUrl = resUrl;
+
+}
+
+}catch(e){}
+
+});
+
 await page.goto(url,{
 waitUntil:"networkidle2",
 timeout:60000
 });
 
-// espera manual
-await new Promise(resolve=>setTimeout(resolve,3000));
+// scroll leve
+await page.evaluate(()=>{
 
-const mp4 = await page.evaluate(()=>{
-
-const html = document.documentElement.innerHTML;
-
-const match = html.match(
-/https:\/\/v1\.pinimg\.com\/videos\/.*?\.mp4/g
-);
-
-if(match && match[0]){
-
-return match[0];
-
-}
-
-const scripts = [
-...document.querySelectorAll("script")
-];
-
-for(const s of scripts){
-
-const txt = s.innerHTML;
-
-const match2 = txt.match(
-/https:\/\/v1\.pinimg\.com\/videos\/.*?\.mp4/g
-);
-
-if(match2 && match2[0]){
-
-return match2[0];
-
-}
-
-}
-
-return null;
+window.scrollBy(0,500);
 
 });
+
+// espera carregar requests
+await new Promise(r=>setTimeout(r,5000));
 
 await browser.close();
 
 res.json({
-mp4
+mp4: videoUrl
 });
 
 }catch(e){
